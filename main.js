2e8e08d7494608c4ae2cc78f1d9c5239347f6aab
leo-dev22 <lndenko@gmail.com>
2022-01-20 22:19:13 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,250 @@
+const graphics4 = document.getElementById("graphics4").getContext("2d");
+let myChart4 = new Chart(graphics4, {
+  type: "pie",
+  data: {
+    labels: [
+      "cat",
+      "dog",
+      "fish",
+      "horse",
+      "chamel",
+      "beef",
+      "cow",
+      "snake",
+      "monkey",
+      "dragon",
+    ],
+    datasets: [
+      {
+        label: "Animals in millions",
+        data: [100, 34, 67, 90, 223, 566, 112, 56, 129, 990],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "red",
+          "crimson",
+          "pupple",
+          "indigo",
+        ],
+        borderColor: [
+          "rgba(25, 99, 132, 0.2)",
+          "rgba(4, 162, 235, 0.2)",
+          "rgba(55, 206, 86, 0.2)",
+          "rgba(5, 192, 192, 0.2)",
+          "rgba(53, 102, 255, 0.2)",
+          "rgba(55, 159, 64, 0.2)",
+
+          "rgba(50, 99, 132, 0.2)",
+          "rgba(23, 162, 235, 0.2)",
+          "rgba(65, 206, 86, 0.2)",
+          "rgba(90, 192, 192, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+const graphics1 = document.getElementById("graphics1").getContext("2d");
+let myChart1 = new Chart(graphics1, {
+  type: "line",
+  data: {
+    labels: [
+      "cat",
+      "dog",
+      "fish",
+      "horse",
+      "chamel",
+      "beef",
+      "cow",
+      "snake",
+      "monkey",
+      "dragon",
+    ],
+    datasets: [
+      {
+        label: "Animals in millions",
+        data: [100, 34, 67, 90, 223, 566, 112, 56, 129, 990],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "red",
+          "crimson",
+          "pupple",
+          "indigo",
+        ],
+        borderColor: [
+          "rgba(25, 99, 132, 0.2)",
+          "rgba(4, 162, 235, 0.2)",
+          "rgba(55, 206, 86, 0.2)",
+          "rgba(5, 192, 192, 0.2)",
+          "rgba(53, 102, 255, 0.2)",
+          "rgba(55, 159, 64, 0.2)",
+
+          "rgba(50, 99, 132, 0.2)",
+          "rgba(23, 162, 235, 0.2)",
+          "rgba(65, 206, 86, 0.2)",
+          "rgba(90, 192, 192, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+const graphics2 = document.getElementById("graphics2").getContext("2d");
+let myChart2 = new Chart(graphics2, {
+  type: "radar",
+  data: {
+    labels: [
+      "cat",
+      "dog",
+      "fish",
+      "horse",
+      "chamel",
+      "beef",
+      "cow",
+      "snake",
+      "monkey",
+      "dragon",
+    ],
+    datasets: [
+      {
+        label: "Animals in millions",
+        data: [100, 34, 67, 90, 223, 566, 112, 56, 129, 990],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "red",
+          "crimson",
+          "pupple",
+          "indigo",
+        ],
+        borderColor: [
+          "rgba(25, 99, 132, 0.2)",
+          "rgba(4, 162, 235, 0.2)",
+          "rgba(55, 206, 86, 0.2)",
+          "rgba(5, 192, 192, 0.2)",
+          "rgba(53, 102, 255, 0.2)",
+          "rgba(55, 159, 64, 0.2)",
+
+          "rgba(50, 99, 132, 0.2)",
+          "rgba(23, 162, 235, 0.2)",
+          "rgba(65, 206, 86, 0.2)",
+          "rgba(90, 192, 192, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+const graphics3 = document.getElementById("graphics3").getContext("2d");
+let myChart3 = new Chart(graphics3, {
+  type: "pie",
+  data: {
+    labels: [
+      "cat",
+      "dog",
+      "fish",
+      "horse",
+      "chamel",
+      "beef",
+      "cow",
+      "snake",
+      "monkey",
+      "dragon",
+    ],
+    datasets: [
+      {
+        label: "Animals in millions",
+        data: [100, 34, 67, 90, 223, 566, 112, 56, 129, 990],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "red",
+          "crimson",
+          "pupple",
+          "indigo",
+        ],
+        borderColor: [
+          "rgba(25, 99, 132, 0.2)",
+          "rgba(4, 162, 235, 0.2)",
+          "rgba(55, 206, 86, 0.2)",
+          "rgba(5, 192, 192, 0.2)",
+          "rgba(53, 102, 255, 0.2)",
+          "rgba(55, 159, 64, 0.2)",
+
+          "rgba(50, 99, 132, 0.2)",
+          "rgba(23, 162, 235, 0.2)",
+          "rgba(65, 206, 86, 0.2)",
+          "rgba(90, 192, 192, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+const graphics5 = document.getElementById("graphics5").getContext("2d");
+let myChart5 = new Chart(graphics5, {
+  type: "bar",
+  data: {
+    labels: [
+      "cat",
+      "dog",
+      "fish",
+      "horse",
+      "chamel",
+      "beef",
+      "cow",
+      "snake",
+      "monkey",
+      "dragon",
+    ],
+    datasets: [
+      {
+        label: "Animals in millions",
+        data: [100, 34, 67, 90, 223, 566, 112, 56, 129, 990],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "red",
+          "crimson",
+          "pupple",
+          "indigo",
+        ],
+        borderColor: [
+          "rgba(25, 99, 132, 0.2)",
+          "rgba(4, 162, 235, 0.2)",
+          "rgba(55, 206, 86, 0.2)",
+          "rgba(5, 192, 192, 0.2)",
+          "rgba(53, 102, 255, 0.2)",
+          "rgba(55, 159, 64, 0.2)",
+
+          "rgba(50, 99, 132, 0.2)",
+          "rgba(23, 162, 235, 0.2)",
+          "rgba(65, 206, 86, 0.2)",
+          "rgba(90, 192, 192, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
